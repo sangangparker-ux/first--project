f7b5994ddcaf010b8ed9d9d9749a738053e53c62
Rufin <SANGANG PARKER@gmail.com>
2026-08-24 08:37:32 +0100

    Angular 22 assignment completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Home } from './home/home';
+import { Contact} from './contact/contact';
+import { NotFound} from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+     path:"", 
+     redirectTo: "home",
+     pathMatch: 'full'
+},
+
+  {
+       path:"about", component:About
+  },
+  {
+    path:"home", component:Home
+  },
+ {
+   path:"contact", component:Contact
+ },
+
+ {
+  path: '**', component:NotFound
+ }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import {Header} from "./header/header";
+import {Main} from "./main/main";
+import {Footer} from "./footer/footer";
+import { Home } from "./home/home";
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
